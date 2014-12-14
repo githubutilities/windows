1bb09f3e89ea3556676116f07cd6351e640c94f6
will <githubutilities@163.com>
2014-12-14 21:53:17 +0800

    path management

--- reg_tools/path_management/import_path.bat
+++ reg_tools/path_management/import_path.bat
@@ -6,15 +6,14 @@ FOR /F "tokens=1-2 delims=	 " %%i in (%CONFIG_FILE%) do (
 	SET VAR_NAME=%%i
 	SET PROGRAM_HOME="%%j"
 	REM delayedexpansion: use ! mark to evaluate at execution time, not parse time.
-	IF EXIST !PROGRAM_HOME! (
-		IF NOT DEFINED !VAR_NAME! (
-			rem echo !VAR_NAME!=!PROGRAM_HOME!
-			@setx !VAR_NAME! !PROGRAM_HOME! 2>nul 1>nul
-		) ELSE (
-			echo !VAR_NAME! already defined.
-		)
-	) ELSE (
-		echo !PROGRAM_HOME! not existed.
+	IF DEFINED !VAR_NAME! (
+		echo Warning: !VAR_NAME! already defined.
+		reg delete HKCU\environment /F /V !VAR_NAME! 1>nul
 	)
+	IF NOT EXIST !PROGRAM_HOME! (
+		echo Warning: !PROGRAM_HOME! not existed.
+	)
+	rem echo !VAR_NAME!=!PROGRAM_HOME!
+	@setx !VAR_NAME! !PROGRAM_HOME! 2>nul 1>nul
 )
 endlocal
