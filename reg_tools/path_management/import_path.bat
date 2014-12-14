3a73a8f2fe322cca6e7b30a64f6bb8f0efe29aab
will <githubutilities@163.com>
2014-12-14 21:05:41 +0800

    path management

--- reg_tools/path_management/import_path.bat
+++ reg_tools/path_management/import_path.bat
@@ -1,13 +1,15 @@
 @echo off
 setlocal enableextensions enabledelayedexpansion
-FOR /F "tokens=1* delims=	" %%i in (env_vars.txt) do (
+set CONFIG_FILE=env_vars.txt
+
+FOR /F "tokens=1* delims=	" %%i in (%CONFIG_FILE%) do (
 	SET VAR_NAME=%%i 
 	SET PROGRAM_HOME="%%j"
 	REM delayedexpansion: use ! mark to evaluate at execution time, not parse time.
 	IF EXIST !PROGRAM_HOME! (
 		IF NOT DEFINED !VAR_NAME! (
-			echo !VAR_NAME!=!PROGRAM_HOME!
-			@setx !VAR_NAME! !PROGRAM_HOME!
+			rem echo !VAR_NAME!=!PROGRAM_HOME!
+			@setx !VAR_NAME! !PROGRAM_HOME! 2>nul 1>nul
 		) ELSE (
 			echo !VAR_NAME! already defined.
 		)
